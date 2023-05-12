b23410274d100597316b18677081c85c301dc888
Anchit Boruah <102992374+anchit98@users.noreply.github.com>
2023-05-12 20:31:46 +0530

    Add files via upload

---
+++ BMI Calculator.py
@@ -0,0 +1,237 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # BMI Calculator
+
+# In[17]:
+
+
+name = input("Enter your name: ")
+
+weight = float(input("Enter your weight in kgs: "))
+height = float(input("Enter your height in m: "))
+
+BMI = (weight) / (height**2)
+
+print(BMI)
+
+if BMI > 0:
+    if(BMI<18.5):
+        print(name +", You are underweight")
+    elif(BMI<=25):
+        print(name +", You are normal weight")
+    elif(BMI<30):
+        print(name +", You are overweight")
+    elif(BMI<35):
+        print(name +", You are obese")
+    elif(BMI<40):
+        print(name +", You are severely obese")
+    else:
+        print("Your are morbidly obese")
+else:
+    print("Enter valid input")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
